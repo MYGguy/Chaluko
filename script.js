b31048e3f1171c1a0c3114983d8030480f527cc9
MYGguy <mygguy613@gmail.com>
2023-03-31 18:36:42 -0400

    fixed the bug: now the whole accordion collapses. new bug: the checkbox stays

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-var acc = document.getElementsByClassName("accordian");
+var acc = document.getElementsByClassName("accordion");
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    let panel = this.nextElementSibling;
+    let nextPanel = panel.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
+      nextPanel.style.display = "none";
     } else {
       panel.style.display = "block";
     }
   });
 }
+
+// //bug: closing the top panel doesn't close the bottom panel.  I want to close the bottom panel when the top panel is closed.
