1094fcabb8f28fa521be62b473a851b95b0a8de5
MYGguy <mygguy613@gmail.com>
2023-04-05 12:19:16 -0400

    fixed! all the panles hide when the top one is clicked

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 var acc = document.getElementsByClassName("accordion");
 
-let panels = document.querySelectorAll("panel");
-
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    //this.classList.toggle("active");
-    // let panel = this.nextElementSibling;
-    // let nextPanel = panel.nextElementSibling;
-    // if (panel.style.display === "block") {
-    //   panel.style.display = "none";
-    //   nextPanel.style.display = "none";
-    // } else {
-    //   panel.style.display = "block";
-    // }
-    this.nextElementSibling.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.classList.contains("hidden")) {
+      panel.classList.remove("hidden");
+    } else {
+      let panels = document.querySelectorAll(".panel");
+
+      panels.forEach((panel) => {
+        panel.classList.add("hidden");
+      });
+    }
   });
 }
-
-// //bug: closing the top panel doesn't close the bottom panel.  I want to close the bottom panel when the top panel is closed.
